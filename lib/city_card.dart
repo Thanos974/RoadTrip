7ddce3d410bd81404ffa3322296ce73d9c552e8d
Emmanuel SCELLES <Escelles@gmail.com>
2021-09-28 18:39:58 +0200

    create widget with state

--- lib/city_card.dart
+++ lib/city_card.dart
@@ -2,51 +2,61 @@ import 'package:flutter/material.dart';
 
 class CityCard extends StatelessWidget {
   const CityCard({Key? key}) : super(key: key);
+  final String name;
+  final String image;
+  final String checked;
+
+  CityCard({this.name, this.image, this.checked});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 5,
-      child: SizedBox(
-        height: 150,
-        child: Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            Image.asset(
-              'assets/images/paris.jpeg',
-              fit: BoxFit.cover,
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                children: <Widget>[
-                  Expanded(
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                      Icon(
-                        Icons.star_border,
-                        size: 40,
-                        color: Colors.white,
-                      ),
-                    ])),
-                  Row(
+        elevation: 5,
+        child: Container(
+          child: SizedBox(
+            height: 150,
+            child: Stack(
+              fit: StackFit.expand,
+              children: <Widget>[
+                Ink.image(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/images/paris.jpeg'),
+                  child: InkWell(
+                    onTap: () {
+                      print('tap 2');
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
                     children: <Widget>[
-                      Text('Paris',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                        )
+                      Expanded(
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                            Icon(
+                              Icons.star_border,
+                              size: 40,
+                              color: Colors.white,
+                            ),
+                          ])),
+                      Row(
+                        children: <Widget>[
+                          Text('Paris',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                              )),
+                        ],
                       ),
                     ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
